b6942adc809d5baef525c19f1f0b1ba3a90507cd
Davi <davigabtomaz@gmail.com>
2023-02-28 16:54:26 -0300

    Lista de Almoço

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './scr/components/Header';
+
+export default function App() {
+
+  const almoco = [
+    {dia: 'segunda-feira', principal: 'Feijoada', sobremesa: 'morango'},
+    {dia: 'terça-feira', principal: 'strogonoff', sobremesa: 'melancia'},
+    {dia: 'quarta-feira', principal: 'Lasanha', sobremesa: 'laranja'},
+    {dia: 'quinta-feira', principal: 'fricasse ', sobremesa: 'iorgute'},
+    {dia: 'sexta-feira', principal: 'risoto', sobremesa: 'gelatina'},
+
+  ]
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <FlatList data={almoco} renderItem={({ item }) => <Text> {item.dia} | Principal: {item.principal} | Sobremesa: {item.sobremesa}</Text>} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
